556182a56fc103e9ee920b2c2924bb5b181923f8
Svetoslav Ivanov <78250184+Svetoslav-Ivanov@users.noreply.github.com>
2021-03-20 21:45:54 +0200

    Add files via upload

---
+++ Stacks and Queues - Exercise/MaximumElement_03.java
@@ -0,0 +1,30 @@
+package Lab_StacksAndQueues.Exercise_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MaximumElement_03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+
+        int n = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            String command = scan.next();
+            switch (command) {
+                case "1":
+                    numbers.push(scan.nextInt());
+                    break;
+                case "2":
+                    numbers.pop();
+                    break;
+                case "3":
+                    System.out.println(Collections.max(numbers));
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Stacks and Queues - Exercise/ReverseNumbersWithAStack_01.java
@@ -0,0 +1,16 @@
+package Lab_StacksAndQueues.Exercise_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseNumbersWithAStack_01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        ArrayDeque<String> input = new ArrayDeque<>();
+        Arrays.stream(scan.nextLine().split("\\s+")).forEach(input::push);
+
+        input.forEach(e -> System.out.print(e + " "));
+    }
+}
